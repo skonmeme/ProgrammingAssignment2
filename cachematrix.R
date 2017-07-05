5853275a4da88bf8e230a165cbf72904b4de99e2
Sung Gon Yi <skonmeme@gmail.com>
2017-07-05 11:34:04 +0900

    Initial implementation of makeCacheMatrix and cacheSolve
    makeCacheMatrix and cacheSolve functions are to improve the efficiency of inverse matrix management. Inverse of matrix spends quiet amount of CPU power, therefore caching is quiet effient to keep CPU power.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+###############################################################################
+# makeCacheMatrix and cacheSolve functions are to improve the efficiency
+# of inverse matrix management. Inverse of matrix spends quiet amount of CPU 
+# power, therefore caching is quiet effient to keep CPU power.
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# make an list object with a matrix and related functions (getter and setter)
+makeCacheMatrix <- function(matrix = matrix()) {
+  inverseMatrix <- NULL
+  set <- function(aMatrix) {
+    matrix <<- aMatrix
+    inverseMatrix <<- NULL
+  }
+  get <- function() matrix
+  setInverse <- function(inverse) inverseMatrix <<- inverse
+  getInverse <- function() inverseMatrix
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# return cached inverse matrix if there is cached nor newly computed (and cached)
+# inverse matrix.
+cacheSolve <- function(cacheMatrix, ...) {
+  inverseMatrix <- cacheMatrix$getInverse()
+  if (!is.null(inverseMatrix)) {
+    message("getting cached inverse matrix")
+    return(inverseMatrix)
+  }
+  matrix <- cacheMatrix$get()
+  inverseMatrix <- solve(matrix, ...)
+  cacheMatrix$setInverse(inverseMatrix)
+  inverseMatrix
 }
